81e87078e6a1c16beb0624670efccef93521c218
dylan jones <dylanjones6@pm.me>
2024-06-29 23:09:17 -0400

    adding some basic librarian functionality and beginning to integrate clap

--- rust/whostolemyhat_learning-projects/library_sim/src/lib.rs
+++ rust/whostolemyhat_learning-projects/library_sim/src/lib.rs
@@ -12,22 +12,22 @@ pub mod book {
     impl Book {
         pub fn new(title: String, author: String,
             year_published: u32, pages: u32) -> Self {
-            Book { title, author, year_published, pages }
+            Self { title, author, year_published, pages }
         }
     }
     
     impl fmt::Display for Book {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             // let len0 = 78;
-            let len1: i32 = self.title.len().try_into().unwrap();
-            let len2: i32 = (4 + self.author.len()).try_into().unwrap();
+            let len1: i32 = self.title.len().try_into().expect("Expected an int");
+            let len2: i32 = (4 + self.author.len()).try_into().expect("Expected an int");
             let year_pub_float: f64 = self.year_published as f64;
             let len3: i32 = 15 + year_pub_float.log10().floor() as i32;
             let pages_float: f64 = self.pages as f64;
             let len4: i32 = 12 + pages_float.log10().floor() as i32;
             let len_vec = vec![&len1, &len2, &len3, &len4];
             // println!("{}", len_vec[0]);
-            let len_max = len_vec.iter().max().unwrap();
+            let len_max = len_vec.iter().max().expect("Expected an int");
             // println!("len1: {}", len1);
             // println!("len2: {}", len2);
             // println!("len3: {}", len3);
@@ -46,19 +46,19 @@ pub mod book {
             write!(f, "|\n").unwrap();
 
             write!(f, "| By: {}", self.author).unwrap();
-            for _ in 0..(**len_max - len2 + 1) {
+            for _ in 0..=**len_max - len2 {
                 write!(f, " ").unwrap();
             }
             write!(f, "|\n").unwrap();
 
             write!(f, "| Published in: {}", self.year_published).unwrap();
-            for _ in 0..(**len_max - len3 + 1) {
+            for _ in 0..=**len_max - len3 {
                 write!(f, " ").unwrap();
             }
             write!(f, "|\n").unwrap();
 
             write!(f, "| {} pages long", self.pages).unwrap();
-            for _ in 0..(**len_max - len4 + 1) {
+            for _ in 0..=**len_max - len4 {
                 write!(f, " ").unwrap();
             }
             write!(f, "|\n").unwrap();
@@ -74,10 +74,11 @@ pub mod book {
 
 pub mod input {
     use clap::{Parser, Subcommand};
+    use clap::{arg, command, Command};
+
     #[derive(Parser)]
     #[command(version, about, long_about = None)]
-
-    struct Cli {
+    pub struct Cli {
         #[command(subcommand)]
         command: Option<Commands>,
     }
@@ -88,16 +89,117 @@ pub mod input {
             list: bool,
         }
     }
+    pub fn command_prompt() -> String {
+        let cmd = clap::command!()
+            .propagate_version(true)
+            // .subcommand_required(true)
+            .arg_required_else_help(true)
+            .subcommand(
+                clap::command!("add")
+                    .about("adds book to book list"),
+                    //.arg(arg!([NAME])),
+            )
+            .subcommand(
+                clap::command!("list")
+                    .about("prints the book list"),
+            );
+            let matches = cmd.get_matches();
+            let matches = match matches.subcommand() {
+                Some(("add", matches)) => {
+                    matches;
+                    // crate::librarian::BookList::add(&book_list);
+                    String::from("add")
+                }
+                Some(("list", matches)) => {
+                    matches;
+                    String::from("list")
+                }
+                // todo
+                Some(("help", matches)) => {
+                    //&cmd.print_help();
+                    String::from("help")
+                
+                }
+                // // todo
+                // Some(("h", matches)) => {
+
+                // }
+                Some(("quit", matches)) => std::process::exit(0),
+                _ => unreachable!("Not add input!"),
+            };
+            matches
+    }
+
 }
 
 
 pub mod librarian {
-    pub fn add() {
-        
+    use std::io;
+    use crate::book::Book;
+
+    pub struct BookList {
+        list: Vec<Book>,
     }
 
-    fn get_name() {
+    impl BookList {
+        pub fn new() -> BookList {
+            BookList {
+                list: Vec::new()
+            }
+        }
 
+        pub fn add(&mut self) {
+            let book_title: String = get_book_content(0);
+            let author: String = get_book_content(1);
+            let year_pub: u32 = get_book_content(2).parse().expect("Could not parse into u32!");
+            let pages: u32 = get_book_content(3).parse().expect("Could not parse into u32!");
+            let book = Book::new(book_title, author, year_pub, pages);
+            println!("{}", &book);
+            self.list.push(book);
+        }
+        
+        pub fn search(&self) -> &Book {
+            let book_title: String = get_book_content(0);
+            for i in &self.list {
+                println!("{i}");
+            }
+            self.list[0]
+        }
+
+        // pub fn remove(&self) -> Book {
+        //     // let book_title: String = get_book
+        // }
+
+        fn write(&self) {
+            // let mut tmpfile: File = temp
+            // for i in self.list {
+
+            // }
+        }
+
+        pub fn list() {
+
+        }
+
+    }
+
+
+
+    fn get_book_content(content_ind: u8) -> String {
+        match content_ind {
+            0 => println!("What is the name of the book?"),
+            1 => println!("What is the name of the author?"),
+            2 => println!("What year was the book published?"),
+            3 => println!("How many pages are in the book?"),
+            _ => eprintln!("Incorrect content_ind given!"),
+        };
+        // println!("What is the name of the book?");
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        stdin.read_line(&mut buffer).expect("Expected a string");
+        let buf_trim = buffer.trim();
+        // println!("{}", &buffer);
+        String::from(buf_trim)
     }
 }
 
--- rust/whostolemyhat_learning-projects/library_sim/src/main.rs
+++ rust/whostolemyhat_learning-projects/library_sim/src/main.rs
@@ -1,19 +1,33 @@
 use library_sim::book::Book;
 use library_sim::input::Cli;
+use library_sim::librarian;
+use clap::{arg, command, Command};
+use library_sim::input::command_prompt;
 
 fn main() {
     // let test = create_book(String::from("Title"), String::from("John Doe"), 2000, 150);
     // println!("{}!", test.author)
     //let mut book_arr: [book; 20];
-    let harry = Book::new("Harry Potter".to_string(),
-                          "J.K. Asshole".to_string(), 1999, 400);
-    let mut book_vec: Vec<Book> = Vec::new();
-    book_vec.push(harry);
+    // let harry = Book::new("Harry Potter".to_string(),
+    //                       "J.K. Asshole".to_string(), 1999, 400);
+    // let mut book_vec: Vec<Book> = Vec::new();
+    // book_vec.push(harry);
     // for i in &book_vec {
     //     println!("{}", i.title);
     // }
-    let hunger_games = Book::new("The Hunger Games".to_string(),
-    "Suzanne Collins".to_string(), 2008, 300);
-    println!("{hunger_games}");
+    // let hunger_games = Book::new("The Hunger Games".to_string(),
+    // "Suzanne Collins".to_string(), 2008, 300);
+    // println!("{hunger_games}");
     // &book_vec.iter().position()
+    
+
+    //librarian::add();
+
+    // let book_list: Vec<&Book> = Vec::new();
+    //let book_list: librarian::BookList = V
+    let book_list = librarian::BookList::new();
+    
+    println!("What command would you like to run?");
+    crate::librarian::BookList::add(book_list);
+    command_prompt();
 }
